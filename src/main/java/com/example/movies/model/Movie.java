b6a942a0e240918f59c469592be26d8812ce1487
trinicorvalan <tcorvalan@cobranzaonline.com>
2026-04-08 20:38:34 -0400

    refactor(movie): simplify Movie class by using Lombok annotations to reduce boilerplate code

--- src/main/java/com/example/movies/model/Movie.java
+++ src/main/java/com/example/movies/model/Movie.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "movie")
 public class Movie {
     @Id
@@ -29,54 +31,6 @@ public class Movie {
         this.director = director;
         this.genre = genre;
         this.year = year;
-        this.synopsis = synopsis;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-    
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getSynopsis() {
-        return synopsis;
-    }
-
-    public void setSynopsis(String synopsis) {
         this.synopsis = synopsis;
     }
 }
